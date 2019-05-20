aeba74a1d653ef32cf898624470645b2b0c7b8bc
Anh Nguyen Tuan <tuananh281106@gmail.com>
2019-05-20 16:14:37 +0700

    no message

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-def chap2:
+def chap2():
     return 9-3, 8*2.5, 9/2, 9/-2. ,9//-2, 9%2, 9.0%2,9%2.0,9%-2,-9%2,9/-2.0,4+3*5,(4+3)*5
 
+def chap2a():
+    temp = 24
+    return temp*1.8+32
+
+def chap2b():
+    x = 10.5
+    y = 4
+    x = x + y
+    return y
+
 chap2()
+chap2a()
+chap2b()
