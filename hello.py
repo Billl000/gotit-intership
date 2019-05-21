5be2772c03a2ba02c4313360c67379730623222d
Anh Nguyen Tuan <tuananh281106@gmail.com>
2019-05-22 00:27:16 +0700

    no message

--- hello.py
+++ hello.py
@@ -1,16 +1,33 @@
-def chap2():
-    return 9-3, 8*2.5, 9/2, 9/-2. ,9//-2, 9%2, 9.0%2,9%2.0,9%-2,-9%2,9/-2.0,4+3*5,(4+3)*5
+def chap3:
+    class practice1():
+        int a = min(2, 3, 4);
+        int b = max(2, -3, 4, 7, -5);
+        int c = max(2, -3, min(4, 7), -5);
+        print(a + ", " + b + ", " + c);
 
-def chap2a():
-    temp = 24
-    return temp*1.8+32
+    class practice2():
+        int a = min(max(3, 4), abs(-5));
+        int b = abs(min(4, 6, max(2, 8)));
+        int c = round(max(5.572, 3.258), abs(-2));
+        print(a + ", " + b + ", " + c);
 
-def chap2b():
-    x = 10.5
-    y = 4
-    x = x + y
-    return y
+    class practice3(index1):
+        return index1 * 3;
 
-chap2()
-chap2a()
-chap2b()
+    class practice4(index1, index2):
+        return abs(index2 - index1);
+
+    class practice5(index1):
+        return index1 / 5 * 8;
+
+    class practice6(index1, index2, index3):
+        return (index1 + index2 + index3) / 3;
+
+    class practice7():
+
+
+
+
+
+
+
