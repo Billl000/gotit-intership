ec33b155e39d21eef24a7a13f04cf85d8fc9d752
Anh Nguyen Tuan <tuananh281106@gmail.com>
2019-05-22 15:17:41 +0700

    no message

--- hello.py
+++ hello.py
@@ -1,38 +1,38 @@
-def chap3:
-    class practice1():
+class chap3:
+    def practice1():
         int a = min(2, 3, 4);
         int b = max(2, -3, 4, 7, -5);
         int c = max(2, -3, min(4, 7), -5);
         print(a + ", " + b + ", " + c);
 
-    class practice2():
+    def practice2():
         int a = min(max(3, 4), abs(-5));
         int b = abs(min(4, 6, max(2, 8)));
         int c = round(max(5.572, 3.258), abs(-2));
         print(a + ", " + b + ", " + c);
 
-    class practice3(index1):
+    def practice3(index1):
         return index1 * 3;
 
-    class practice4(index1, index2):
+    def practice4(index1, index2):
         return abs(index2 - index1);
 
-    class practice5(index1):
+    def practice5(index1):
         return index1 / 5 * 8;
 
-    class practice6(index1, index2, index3):
+    def practice6(index1, index2, index3):
         return mean(index1 + index2 + index3, 3);
 
-    class practice7(index1, index2, index3, index4):
+    def practice7(index1, index2, index3, index4):
         int sum = sum(index1, index2, index3, index4);
         int Max_Total = sum - min(index1, index2, index3, index4);
         return Max_Total / 3;
 
 
-    class practice8(day1, day2):
+    def practice8(day1, day2):
         return int(abs(day2 - day1) / 7);
 
-    class practice9and10(num):
+    def practice9and10(num):
         return pow(num, 2);
 
 
