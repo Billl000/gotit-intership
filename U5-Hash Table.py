2a642e4acbcec2ddb8b19478f5540756f119be8c
Billl000 <50823870+Billl000@users.noreply.github.com>
2020-07-19 14:21:39 +0700

    Update U5-Hash Table.py

--- U5-Hash Table.py
+++ U5-Hash Table.py
@@ -2,6 +2,6 @@ dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 del dict['Name']; # remove entry with key 'Name'
 dict.clear();     # remove all entries in dict
 del dict ;        # delete entire dictionary
-print dict['Age']
+
 print ("dict['Age']: ", dict['Age'])
 print ("dict['School']: ", dict['School'])
