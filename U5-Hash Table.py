05d3799db936a96ef11ec2c7a55c5b29a0150c74
Billl000 <50823870+Billl000@users.noreply.github.com>
2020-08-01 16:30:03 +0700

    Update U5-Hash Table.py

--- U5-Hash Table.py
+++ U5-Hash Table.py
@@ -1,4 +1,7 @@
 info = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 
+print(info.keys())
+print(info.values())
+print(info.items())
 print ("info['Name']: ", info['Name'])
 print ("info['Age']: ", info['Age'])
